d253b4583c9085816625efac55f5706ff05c0b0c
Steve Boyd <emteknetnz@gmail.com>
2025-08-19 10:43:55 +1200

    .

--- backup.php
+++ backup.php
@@ -1,27 +1,2 @@
 <?php
 
-use SilverStripe\SupportedModules\MetaData;
-
-$vendorDir = dirname(dirname(__DIR__));
-
-include $vendorDir . '/autoload.php';
-
-$jsModules = [];
-
-$metadata = MetaData::getAllRepositoryMetaData(false);
-
-foreach ($metadata as $data) {
-    $subdir = $data['packagist'] ?? null;
-    if (!$subdir) {
-        continue;
-    }
-    if (file_exists("$vendorDir/$subdir/package.json")) {
-        $jsModules[] = $subdir;
-    }
-}
-
-sort($jsModules);
-
-foreach ($jsModules as $module) {
-    echo $module . PHP_EOL;
-}
--- src/Commands/DefaultCommand.php
+++ src/Commands/DefaultCommand.php
@@ -2,6 +2,7 @@
 
 namespace emteknetnz\JsUpdater\Commands;
 
+use SilverStripe\SupportedModules\MetaData;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,12 +12,28 @@ use Symfony\Component\Console\Output\OutputInterface;
     name: 'update-js',
     description: 'The default command.',
 )]
-class DefaultCommand extends Command
+class UpdateJsCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('Hello from the default command!');
-
+        $vendorDir = dirname(dirname(__DIR__));
+        $jsModules = [];
+        $metadata = MetaData::getAllRepositoryMetaData(false);
+        var_dump($metadata);die;
+        foreach ($metadata as $data) {
+            $subdir = $data['packagist'] ?? null;
+            if (!$subdir) {
+                continue;
+            }
+            if (file_exists("$vendorDir/$subdir/package.json")) {
+                $jsModules[] = $subdir;
+            }
+        }
+        sort($jsModules);
+        foreach ($jsModules as $module) {
+            echo $module . PHP_EOL;
+        }
         return Command::SUCCESS;
     }
 }
